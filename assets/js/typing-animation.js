bb5cb0492251a04ccc77d6129df6964ad9175942
abbas aghebaty <abbas.aghebaty@gmail.com>
2026-09-13 18:58:22 +0330

    Sync typing animation with skills

--- assets/js/typing-animation.js
+++ assets/js/typing-animation.js
@@ -1,46 +1,57 @@
 // Rolling-title typing/deleting animation under the hero name.
 export function initTypingAnimation(typingTextEl) {
-        // ========================================
-        // Typing Animation
-        // ========================================
-        const titles = [
-            'JavaScript Developer',
-            'Web Developer',
-            'Open Source Creator',
-            'Front-end Developer',
-            'Telegram Bot Developer',
-        ];
-        let titleIndex = 0;
-        let charIndex = 0;
-        let isDeleting = false;
-        const typingPause = 1800;
-        const typingSpeed = 65;
-        const deletingSpeed = 30;
+    const titles = [
+        'JavaScript',
+        'HTML & CSS',
+        'Front-end Development',
+        'Back-end Development',
+        'Telegram Bot Development',
+        'Telegram Mini Apps',
+        'Responsive Web Design',
+        'UI Design',
+        'Graphic Design',
+        'Thumbnail Design',
+        'Photo Editing',
+        'Open Source',
+    ];
 
-        function typeAnimation() {
-            const currentTitle = titles[titleIndex];
-            if (isDeleting) {
-                charIndex--;
-                typingTextEl.textContent = currentTitle.substring(0, charIndex);
-                if (charIndex <= 0) {
-                    isDeleting = false;
-                    titleIndex = (titleIndex + 1) % titles.length;
-                    setTimeout(typeAnimation, 250);
-                    return;
-                }
-                setTimeout(typeAnimation, deletingSpeed);
-            } else {
-                charIndex++;
-                typingTextEl.textContent = currentTitle.substring(0, charIndex);
-                if (charIndex >= currentTitle.length) {
-                    isDeleting = true;
-                    setTimeout(typeAnimation, typingPause);
-                    return;
-                }
-                const speed = typingSpeed + Math.random() * 35;
-                setTimeout(typeAnimation, speed);
+    let titleIndex = 0;
+    let charIndex = 0;
+    let isDeleting = false;
+    const typingPause = 1800;
+    const typingSpeed = 65;
+    const deletingSpeed = 30;
+
+    function typeAnimation() {
+        const currentTitle = titles[titleIndex];
+
+        if (isDeleting) {
+            charIndex--;
+            typingTextEl.textContent = currentTitle.substring(0, charIndex);
+
+            if (charIndex <= 0) {
+                isDeleting = false;
+                titleIndex = (titleIndex + 1) % titles.length;
+                setTimeout(typeAnimation, 250);
+                return;
             }
-        }
-        setTimeout(typeAnimation, 400);
 
+            setTimeout(typeAnimation, deletingSpeed);
+            return;
+        }
+
+        charIndex++;
+        typingTextEl.textContent = currentTitle.substring(0, charIndex);
+
+        if (charIndex >= currentTitle.length) {
+            isDeleting = true;
+            setTimeout(typeAnimation, typingPause);
+            return;
+        }
+
+        const speed = typingSpeed + Math.random() * 35;
+        setTimeout(typeAnimation, speed);
+    }
+
+    setTimeout(typeAnimation, 400);
 }
